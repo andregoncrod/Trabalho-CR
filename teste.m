0f70ead716b7d5760509c5075814b1407702e85d
André Gonçalves Rodrigues <andrerodrigues.pombal@gmail.com>
2013-03-23 20:55:03 +0000

    ficheiros de teste novos
    testei com varios tabuleiros e funca :D

--- teste.m
+++ teste.m
@@ -2,11 +2,11 @@ function NUMS=teste()
 
 clc;
 
-[a, map] = imread('sudoku1.bmp');
+[a, map] = imread('6a00d83426b77b53ef00e54f3ffcb08833-800wi.bmp');
 
 matriz = limpa_matriz(a);
 
-imwrite(matriz, 'Teste_2', 'BMP');
+imwrite(matriz, 'Teste_1', 'BMP');
 
 NUMS = percorre_grelha(matriz);
 
